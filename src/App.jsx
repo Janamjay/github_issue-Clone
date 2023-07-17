4d99203b213a61d1773e79817ea0fd18266d098f
Janammjay <janammjayk6@gmail.com>
2023-07-17 21:11:47 +0530

    navbar completed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './component/navbar'
+
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import nav from "./navbar.module.css";
+import logo from "../images/logo.png";
+import browser from "../images/browser.png";
+const Navbar = () => {
+  return (
+    <nav className={nav.main}>
+      <div className={nav.left}>
+        <div className={nav.logogit}>
+          <img className={nav.logo} src={logo} alt="logo" />
+          <li>GitHub Docs</li>
+        </div>
+      </div>
+      <div className={nav.right}>
+        <input type="text" />
+        <img className={nav.logo}  src={browser} alt="browser" />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
