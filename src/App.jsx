6e88ccec0c8c78892d8b20130e7d01ff680c3b03
Janammjay <janammjayk6@gmail.com>
2023-07-17 23:27:36 +0530

    sidebar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Navbar from "./component/navbar";
 import SidebarHeading from "./component/SidebarHeading";
+import "./App.css";
+import Sidebar from "./component/Sidebar";
 
 function App() {
   return (
     <>
       <Navbar />
-      <SidebarHeading/>
+      <div className="sidebar">
+        <SidebarHeading />
+        <Sidebar />
+      </div>
     </>
   );
 }
---
+++ src/component/Sidebar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {data} from '../util/Data'
+import side from './Sidebar.module.css'
+
+const Sidebar = () => {
+    
+  return (
+    <div className={side.main_side}>
+      {data.map((elem,ind)=>{
+        return(
+        <div className={side.data} key={ind}>
+            <p >{elem}</p>
+        </div>
+      )})}
+    </div>
+  )
+}
+
+export default Sidebar
